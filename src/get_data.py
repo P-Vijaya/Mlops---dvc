4a689c8b9450b5010a272a0c7e03bcd05ce7c31f
P-Vijaya <vijayapadmanabha4@gmail.com>
2022-02-19 19:02:41 +0530

    created  get_data and load_data

--- src/get_data.py
+++ src/get_data.py
@@ -6,6 +6,7 @@ import yaml
 import pandas as pd
 import argparse
 
+
 def read_params(config_path):
     with open(config_path) as yaml_file:
         config = yaml.safe_load(yaml_file)
@@ -13,13 +14,25 @@ def read_params(config_path):
 
 def get_data(config_path):
     config = read_params(config_path)
-    ##print(config)
+    #print(config)
     data_path = config["data_source"]["s3_source"]
-    df = pd.read_csv(data_path,sep=",",encoding='utf-8')
+    #data_path = '../../Mlops-task/'+data_path
+    data_path = '../'+data_path
+    #data_path = 'data_given/Admission_Prediction.csv'
+    df = pd.read_csv(data_path, sep=",", encoding='utf-8')
     return df
 
-if __name__ == "__main__":
+
+
+if __name__=="__main__":
     args = argparse.ArgumentParser()
-    args.add_argument("--config",default="params.yaml")
+    args.add_argument("--config",default='../../Mlops-task/params.yaml')
     parsed_args = args.parse_args()
     data = get_data(config_path=parsed_args.config)
+
+
+#with open('../../Mlops-task/params.yaml') as f:
+#    data = yaml.load(f)
+#    print(data)
+
+#with open('../data_given/Admission_Prediction.csv')
