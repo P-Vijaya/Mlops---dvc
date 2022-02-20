0e2e943733499674a190e87fbe46efb9c6e4a350
P-Vijaya <vijayapadmanabha4@gmail.com>
2022-02-20 18:23:59 +0530

    created tox

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    description="its a admission prediction package",
+    author="vijaya",
+    packages=find_packages(),
+    license="MIT"
+)
